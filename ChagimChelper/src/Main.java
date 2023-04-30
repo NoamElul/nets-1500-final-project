7f96073c237850733951d5c94485d8c37ce5bb2b
N.E <nelul@seas.upenn.edu>
2023-04-30 15:41:00 -0400

    Refactor main, clean up date/time formatting

--- ChagimChelper/src/Main.java
+++ ChagimChelper/src/Main.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
-import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,63 +12,107 @@ import java.nio.file.Path;
 public class Main {
     private Schedule schedule;
     private List<HebCalAPI.HolidayInterval> holidays;
-    boolean anyConflicts = false;
-    HashMap<String, List<String>> conflictsPerCourse;
-    String name = "";
-
+    private List<Conflict> conflicts;
 
     public static void main(String[] args) throws Throwable {
-        System.out.println("Hello! \nTo start please enter your name: ");
-        String name = (new Scanner(System.in)).nextLine();
-        Main obj = new Main(name);
+        Main mainObj = new Main();
         System.out.println("Welcome to Chagim Chelper: a tool to help you track which classes you may " +
                 "miss for Jewish holidays.");
         System.out.println("We need to know which classes you are in, so please go on PennCoursePlan and download the .ics file or .ics link with your schedule.");
         System.out.println("Alternatively, you can download the .ics of a Google Calendar with just your classes by going to settings and downloading your schedule using the provided .ics secret address.");
-        obj.testBoth();
+        mainObj.setupAndPrompt();
+        mainObj.printConflicts();
         System.out.print("Would you like us to generate emails you can send to your professors (Y/N)? ");
         String response = (new Scanner(System.in)).nextLine();
         if (response.contains("Y") || response.contains("y")) {
-            obj.generateEmails();
+            mainObj.generateEmails();
         } else {
             System.out.print("Okay, thank you for using Chagim Chelper!");
         }
     }
 
-    public Main(String name) {
-        this.name = name;
+    private record Conflict(HebCalAPI.HolidayInterval holiday, List<Schedule.CourseMeeting> courseMeetings) {}
+
+    private void setupAndPrompt() throws Throwable {
+        System.out.print("Enter the filepath or URL to the .ics file with your schedule: ");
+        String path = (new Scanner(System.in)).nextLine();
+        while (path.startsWith("\"") && path.endsWith("\"")) {
+            path = path.substring(1, path.length() - 1);
+        }
+        if (path.startsWith("http")) {
+            URI url = URI.create(path);
+            System.out.print("You entered: ");
+            System.out.println(path);
+
+            this.schedule = new Schedule(url);
+        } else {
+            Path filepath = Path.of(path);
+            System.out.print("You entered: ");
+            System.out.println(filepath);
+
+            this.schedule = new Schedule(filepath);
+        }
+
+        this.holidays = HebCalAPI.getHolidays(schedule.startDate.minusDays(7), schedule.endDate.plusDays(7));
+
+        this.conflicts = new ArrayList<>();
+        for (var h : holidays) {
+            var newConflicts = schedule.meetingsInInterval(h.interval);
+            if (!newConflicts.isEmpty()) {
+                this.conflicts.add(new Conflict(h, newConflicts));
+            }
+        }
     }
+
+    private void printConflicts() throws Throwable {
+        for (var c : conflicts) {
+            if (!c.courseMeetings().isEmpty()) {
+                System.out.println();
+            }
+            for (var m: c.courseMeetings()) {
+                System.out.println("The course " + m.courseName() + " meeting from " + timeSlotString(m.meetingTime())
+                        + " on " + dateSlotString(m.meetingTime()) + " conflicts with the holiday of " + c.holiday().eventName + ".");
+            }
+        }
+        System.out.println();
+        if (conflicts.isEmpty()) {
+            System.out.println("There were no conflicts with your schedule");
+        }
+    }
+
 
     private void generateEmails() throws Throwable {
         //FINISH IMPLEMENTING
+        System.out.println("Please enter your name: ");
+        String name = (new Scanner(System.in)).nextLine();
         File file = new File("chagimChelperEmails.txt");
         try {
-            file.createNewFile();
+            boolean result = file.createNewFile();
+            if (!result) {
+                System.out.println("Error occurred creating email file.");
+            }
         } catch (IOException e) {
             System.out.println("Error occurred creating email file.");
         }
 
         FileWriter fileWriter = new FileWriter("chagimChelperEmails.txt");
 
-        conflictsPerCourse = new HashMap<String, List<String>>();
-        for (var h : holidays) {
-            var conflicts = schedule.meetingsInInterval(h.interval);
-            if (!conflicts.isEmpty()) {
+        HashMap<String, List<String>> conflictsPerCourse = new HashMap<>();
+        for (var conf : this.conflicts) {
+            if (!conf.courseMeetings.isEmpty()) {
                 System.out.println();
-                anyConflicts = true;
             }
-            for (var c: conflicts) {
+            for (var c: conf.courseMeetings()) {
                 String course = c.courseName();
                 if (conflictsPerCourse.containsKey(course)) {
                     List<String> existingConflicts = conflictsPerCourse.get(course);
-                    existingConflicts.add("I will be missing class on " + dateSlot(c.meetingTime()) + " for the holiday of " + h.eventName + ".");
+                    existingConflicts.add("I will be missing class on " + dateSlotString(c.meetingTime()) + " for the holiday of " + conf.holiday().eventName + ".");
                     conflictsPerCourse.put(course, existingConflicts);
                 } else {
                     List<String> newConflictList = new ArrayList<String>();
-                    newConflictList.add("I will be missing class on " + dateSlot(c.meetingTime()) + " for the holiday of " + h.eventName + ".");
+                    newConflictList.add("I will be missing class on " + dateSlotString(c.meetingTime()) + " for the holiday of " + conf.holiday().eventName + ".");
                     conflictsPerCourse.put(course, newConflictList);
                 }
-
             }
         }
 
@@ -102,87 +146,23 @@ public class Main {
         fileWriter.close();
     }
 
-    private void testBoth() throws Throwable {
-        System.out.print("Enter the filepath or URL to the .ics file with your schedule: ");
-        String path = (new Scanner(System.in)).nextLine();
-        while (path.startsWith("\"") && path.endsWith("\"")) {
-            path = path.substring(1, path.length() - 1);
-        }
-        if (path.startsWith("http")) {
-            URI url = URI.create(path);
-            System.out.print("You entered: ");
-            System.out.println(path);
 
-            this.schedule = new Schedule(url);
+    private String timeSlotString(Interval interval) {
+        // For 24-hour clock format instead of am/pm, replace "hh:mm a" with "HH:mm"
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("hh:mm a");
+        return dtf.format(interval.start) + " to " + dtf.format(interval.end);
+    }
+
+    private static String dateSlotString(Interval interval) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd");
+        String startDateString = dtf.format(interval.start);
+        String endDateString = dtf.format(interval.end);
+        if (startDateString.equals(endDateString)) {
+            return startDateString;
         } else {
-            Path filepath = Path.of(path);
-            System.out.print("You entered: ");
-            System.out.println(filepath);
-
-            this.schedule = new Schedule(filepath);
-        }
-
-        this.holidays = HebCalAPI.getHolidays(schedule.startDate.minusDays(7), schedule.endDate.plusDays(7));
-
-        for (var h : holidays) {
-            var conflicts = schedule.meetingsInInterval(h.interval);
-            if (!conflicts.isEmpty()) {
-                System.out.println();
-                this.anyConflicts = true;
-            }
-            for (var c: conflicts) {
-                System.out.println("The course " + c.courseName() + " meeting from " + timeSlot(c.meetingTime())
-                        + " on " + dateSlot(c.meetingTime()) + " conflicts with the holiday of " + h.eventName + ".");
-
-            }
-        }
-        System.out.println();
-        if (!anyConflicts) {
-            System.out.println("There were no conflicts with your schedule");
+            // Realistically, this shouldn't occur for actual classes (they don't go across multiple days)
+            return startDateString + " & " + endDateString;
         }
     }
 
-    private String timeSlot(Interval interval) {
-        String intervalString = interval.toString();
-
-        int timeStartIndex = intervalString.indexOf("start=");
-        int timeEndIndex = intervalString.indexOf("end=");
-        String time = intervalString.substring(timeStartIndex + 17, timeStartIndex + 22) + "-" +
-                intervalString.substring(timeEndIndex + 15, timeEndIndex + 20);
-
-
-        return time;
-    }
-
-    private static String dateSlot(Interval interval) {
-        String intervalString = interval.toString();
-
-        int dateStartIndex = intervalString.indexOf('-');
-        String date = intervalString.substring(dateStartIndex + 1, dateStartIndex + 3) + "/"
-                + intervalString.substring(dateStartIndex + 4, dateStartIndex + 6);
-
-        return date;
-    }
-
-    private void testAPI() throws Throwable {
-        var holidays = HebCalAPI.getHolidays(LocalDate.parse("2023-02-01"), LocalDate.parse("2023-04-23"));
-        System.out.println(holidays);
-    }
-
-    private  void testSchedule() throws Throwable {
-        System.out.print("Enter the path to the .ics file with your schedule: ");
-        String filename = (new Scanner(System.in)).nextLine();
-        while (filename.startsWith("\"") && filename.endsWith("\"")) {
-            filename = filename.substring(1, filename.length() - 1);
-        }
-        Path filepath = Path.of(filename);
-        System.out.print("You entered: ");
-        System.out.println(filepath);
-
-        var schedule = new Schedule(filepath);
-
-        for (var c : schedule.courses) {
-            System.out.println(c);
-        }
-    }
 }
